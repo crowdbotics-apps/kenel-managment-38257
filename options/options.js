32fd338f18236f026b8187c7b930bec57201ecee
Crowdbotics <team@crowdbotics.com>
2023-01-11 20:25:59 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "kenel-managment-38257",
+  url: "https://kenel-managment-38257.botics.co",
+  api: "https://kenel-managment-38257.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
